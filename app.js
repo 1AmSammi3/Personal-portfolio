4e130cd2376f28c02c0fed92b75ef03f59c4b05e
Sammie <osenisamad2004@gmail.com>
2023-07-13 14:58:56 +0100

    Updated

--- app.js
+++ app.js
@@ -24,3 +24,34 @@ for (let link of links) {
 but.addEventListener("click", back);
 ham.addEventListener("click", show);
 
+function handleIntersection(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    } else {
+      entry.target.classList.remove("active");
+    }
+  });
+}
+
+const sectionObserver = new IntersectionObserver(handleIntersection, {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.2 // Adjust the threshold as needed
+});
+
+const sectionElements = document.querySelectorAll("section");
+sectionElements.forEach(section => {
+  sectionObserver.observe(section);
+});
+
+const footerObserver = new IntersectionObserver(handleIntersection, {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.2 // Adjust the threshold as needed
+});
+
+const footerElements = document.querySelectorAll("footer");
+footerElements.forEach(footer => {
+  footerObserver.observe(footer);
+});
